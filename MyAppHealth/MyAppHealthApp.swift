dad5bc894c186db30ed8091f141e933c87455057
MaxG0312 <maxencegelin2@gmail.com>
2025-02-18 08:38:49 +0100

    Initial Commit

---
+++ MyAppHealth/MyAppHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppHealthApp.swift
+//  MyAppHealth
+//
+//  Created by gelin maxence on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyAppHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
