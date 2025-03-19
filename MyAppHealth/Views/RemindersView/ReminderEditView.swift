bfb35a0795852b7d5b692d2b7a56504361dd1b0b
MaxG0312 <maxencegelin2@gmail.com>
2025-03-19 10:46:03 +0100

    b

---
+++ MyAppHealth/Views/RemindersView/ReminderEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderEditView.swift
+//  MyAppHealth
+//
+//  Created by gelin maxence on 19/03/2025.
+//
+
+import SwiftUI
+
+struct ReminderEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderEditView()
+}
